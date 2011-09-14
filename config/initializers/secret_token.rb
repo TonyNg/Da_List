90885b6be71db6e6dc0110a2171cd23eaf20b3b6
Tony Ng <tonyng222@gmail.com>
2011-09-13 20:10:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DaList::Application.config.secret_token = '3bcbea4c78727773ff4e69b83a9cc00d409d3a8048dc53ee4dd54ccfb9de661c1e1942982d9344611eec6ef475e7b437bb9cba08a052dde3ab2d762fd3a3baf1'
