bd5a5fc8c07b6e9b164f4ec1c461063a0cffbde3
Tony Ng <tonyng222@gmail.com>
2011-09-13 21:19:18 -0700

    Added a Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 DaList::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+  
+  root :to => "pages#home"
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
